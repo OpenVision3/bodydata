115cb621ef4d0f129711887cc5235e23c45cdf60
Luke Liu <honglin.liu@posecap.com>
2026-03-09 14:41:40 -0400

    update package.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let version = "1.0.0"
-let base    = "https://github.com/OpenVision3/bodytracking/releases/\(version)"
+let base    = "https://github.com/OpenVision3/bodytracking/releases/download/\(version)"
 
 let package = Package(
     name: "BodyTracking",
@@ -15,12 +15,12 @@ let package = Package(
         .binaryTarget(
             name: "Core",
             url: "\(base)/Core.xcframework.zip",
-            checksum: "cbe441515e5decc9071557b84ae7d6dd9c9e447d287884e47814c52aab765a23"
+            checksum: "761afff7d32d36579baad1d48ece9fd34f3aa2635851b6818e01c2dc9660e32e"
         ),
         .binaryTarget(
             name: "File",
             url: "\(base)/File.xcframework.zip",
-            checksum: "c02bcc4bd2b024ba63256e3e74599e787b46afa434963c516301b2230822c1af"
+            checksum: "d98cf82d70613da68f8499d1756227078cd890772cf8b8cb2393a2eea29e3c28"
         ),
     ]
 )
